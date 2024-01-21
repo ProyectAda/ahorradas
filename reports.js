20cb0071cdd5b7af9e9c9508d09db868eb6d339a
Sandra Flores <sfloresq07@gmail.com>
2024-01-21 16:47:03 -0300

    hotfit balance

--- reports.js
+++ reports.js
@@ -54,7 +54,7 @@ const getCategoryWithMaxProfit = (operaciones) => {
         }
     }
 
-    let maxProfitCategory = {};
+    let maxProfitCategory = null;
     let maxProfit = 0;
 
     for (const categoria in categoriasGanancia) {
@@ -91,7 +91,7 @@ const getCategoryWithMaxExpense = (operaciones) => {
         }
     }
 
-    let maxExpenseCategory = {};
+    let maxExpenseCategory = null;
     let maxExpense = 0;
 
     for (const categoria in categoriasGasto) {
